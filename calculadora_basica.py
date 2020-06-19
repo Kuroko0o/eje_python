b3c5f491a0cb5d0c728039dff81e11bd8bd31134
Kuroko0o <gutierrez.garay.rolando@gmail.com>
2020-06-18 22:04:00 -0300

    Se creo una calculadora basica

---
+++ calculadora_basica.py
@@ -0,0 +1,19 @@
+calculo = input("Ingresar el simbolo de la operación que desea realizar: (+,-,*,/) \n")
+
+num = input("Ingresar el número a calcular: \n")
+num_dos = input("Ingresar el siguiente número a calcular: \n")
+try:
+    n = int(num)
+    nu = int(num_dos)
+except:
+    print("El Valor ingresado no es un número entero")
+    quit()
+
+if calculo == "+":
+    print("El resultado de la suma es: \n", n + nu)
+elif calculo == "-":
+    print("El resultado de la resta es: \n", n - nu)
+elif calculo == "*":
+    print("El resultado de la multiplicación es: \n", n * nu)
+elif calculo == "/":
+    print("El resultado de la división es: \n", n / nu)
