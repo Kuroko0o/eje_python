24821b8df86a0a7b44de49a0bf1a4436f2b295b1
Kuroko0o <gutierrez.garay.rolando@gmail.com>
2020-06-19 20:34:51 -0300

    Se agregaron detalles visuales

--- calculadora_basica.py
+++ calculadora_basica.py
@@ -1,4 +1,5 @@
-
+print ("  ****** Bienvenido a Calculadora Basica 2020 ******   ")
+print (" -------------------------------------------------------- ")
 num = input("Ingresar el número entero a calcular: \n")
 calculo = input("Ingresar el simbolo de la operación que desea realizar: (+,-,*,/) \n")
 num_dos = input("Ingresar el siguiente número a calcular: \n")
@@ -7,6 +8,7 @@ try:
     n = float(num)
     nu = float(num_dos)
 except:
+    print(" -------------------------------------------------------- ")
     print("El Valor ingresado no es un número")
     quit()
 
@@ -20,5 +22,8 @@ elif calculo == "*":
 elif calculo == "/":
     print("El resultado de la división es: \n", n / nu)
 else:
+    print(" ------------------------------------------------------- ")
     print ("El valor ingresado no es un simbolo de operación matematica")
-    
+
+print (" ---------------------------------------------------------- ")
+print ("  ****** Gracias por usar Calculadora Basica 2020 ******")
